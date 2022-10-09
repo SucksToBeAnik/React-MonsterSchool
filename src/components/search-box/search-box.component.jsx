8b4d7c71f3528993b218388767ae3c9c0f6d6c66
SucksToBeAnik <anik.islam1494@gmail.com>
2022-10-09 23:23:15 +0600

    Initial commit of function based component

--- src/components/search-box/search-box.component.jsx
+++ src/components/search-box/search-box.component.jsx
@@ -1,12 +1,9 @@
-import { Component } from "react";
 import './search-box.styles.css'
 
-class SearchBox extends Component{
-    render(){
+const SearchBox = (props)=>{
         return(
-                <input className={`search-box ${this.props.className}`} type='search' placeholder={this.props.placeholder} onChange={this.props.onChangeHandler} />
+                <input className={`search-box ${props.className}`} type='search' placeholder={props.placeholder} onChange={props.onChangeHandler} />
         )
-    }
 }
 
 export default SearchBox
